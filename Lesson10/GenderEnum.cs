2ab4a6e33890b2036d9e62c62ac65d525ff1cf2b
Rif <Rifus91@mail.ru>
2021-03-23 00:10:55 +0300

    добавили перечисления Enum для дней недели и гендера, поправили в программе

---
+++ Lesson10/GenderEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace Lesson10
+{
+    enum GenderEnum
+    {
+        Man = 1,
+        Women = 0
+    }
+}
--- Lesson10/Program.cs
+++ Lesson10/Program.cs
@@ -10,15 +10,15 @@ namespace Lesson10
         static void Main(string[] args)
         {
             //ввод от клиента, пол и имя
-            // Step1(false, "Иванова Ольга");
-            //Step2(true, "Петров Иван");
-            StepWeek1(8);
+             //Step1(GenderEnum.Women, "Иванова Ольга");
+            
+            StepWeek1((DayOfWeekEnum)5);
         }
 
        
-        static void Step1(bool gender, string name)
+        static void Step1(GenderEnum gender, string name)
         {
-            if (gender == Gender.isMan)
+            if (gender == GenderEnum.Man)
             {
                 Console.WriteLine($"Уважаемый, {name}!");
             }
@@ -29,12 +29,12 @@ namespace Lesson10
             Console.WriteLine("Приветствуем на нашем сайте!");
         }
 
-        static void StepWeek1(byte dayOfWeek)
+        static void StepWeek1(DayOfWeekEnum dayOfWeek)
         {
-            if (dayOfWeek == DayOfWeek.Saturday || dayOfWeek == DayOfWeek.Sanday)
-            { Console.WriteLine("Выходной день"); }
+            if (dayOfWeek == DayOfWeekEnum.Saturday || dayOfWeek == DayOfWeekEnum.Sanday)
+            { Console.WriteLine($"{dayOfWeek} - Выходной день"); }
             else
-            { Console.WriteLine("Рабочий день"); }
+            { Console.WriteLine($"{dayOfWeek} - Рабочий день"); }
         }
 
        
