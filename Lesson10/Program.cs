b7c801a0c7f75923f0a7847f022607993d2235d2
Rif <Rifus91@mail.ru>
2021-03-22 12:20:29 +0300

    Add project files.

---
+++ Lesson10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lesson10
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            //ввод от клиента, пол и имя
+            Step1(false, "Иванова Ольга");
+            //Step1(true, "Петров Иван");
+        }
+
+        static void Step1(bool gender, string name)
+        {
+            if (gender == true) // муж. - true, жен. - false
+            {
+                Console.WriteLine($"Уважаемый, {name}!");
+            }
+            else 
+            {
+                Console.WriteLine($"Уважаемая, {name}!");
+            }
+            Console.WriteLine("Приветствуем на нашем сайте!");                
+        }
+        
+
+    }
+}
