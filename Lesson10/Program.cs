861968f45474ad5cccfd3ffa3cc653d75b04556e
Rif <Rifus91@mail.ru>
2021-03-22 12:26:05 +0300

    добавили глобальные переменные isMan, isWomen. и вторую версию проверки

--- Lesson10/Program.cs
+++ Lesson10/Program.cs
@@ -4,11 +4,12 @@ namespace Lesson10
 {
     class Program
     {
-       
+        public static bool isMan = true;
+        public static bool isWomen = false;
         static void Main(string[] args)
         {
             //ввод от клиента, пол и имя
-            Step1(false, "Иванова Ольга");
+            Step2(false, "Иванова Ольга");
             //Step1(true, "Петров Иван");
         }
 
@@ -24,7 +25,20 @@ namespace Lesson10
             }
             Console.WriteLine("Приветствуем на нашем сайте!");                
         }
-        
+
+        static void Step2(bool gender, string name)
+        {
+            if (gender == isMan) 
+            {
+                Console.WriteLine($"Уважаемый, {name}!");
+            }
+            else
+            {
+                Console.WriteLine($"Уважаемая, {name}!");
+            }
+            Console.WriteLine("Приветствуем на нашем сайте!");
+        }
+
 
     }
 }
